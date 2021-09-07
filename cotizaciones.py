c977f82421ce6fbfccf1471302fe347cf84f07f9
Francisco Galindez <frangalindez7@gmail.com>
2021-09-07 17:13:37 -0300

    commit 2

--- cotizaciones.py
+++ cotizaciones.py
@@ -1,5 +1,19 @@
 if __name__ == '__main__':
     print("Cotizaciones") 
 
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
 
-#Máximos y mínimos
+#Identificar las 2 cotizaciones de mayor ganancia y de mayor perdida.
+
+#si los cotizaciones están en una lista:
+
+class bolsa_valores:
+    def __init__(self, nombre , max_ganancia , min_ganancia):
+        self.nombre = nombre
+        self.max_ganancia = max_ganancia
+        self.min_ganancia = min_ganancia
+
+
