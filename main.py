cb2507caebedd0823b0fbf8744e3644335ac8f9c
Renzo Condormango <renzoc@users.noreply.github.com>
2021-09-07 22:48:24 -0300

    commit_renzo 3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import csv
 from datetime import datetime
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url_page = 'http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx'
 
@@ -10,7 +11,7 @@ soup = BeautifulSoup(page, "lxml")
 
 table = soup.find("table", {'id': 'ctl00_Contenido_tblÍndices'})
 name = ""
-price = ""
+porcent = ""
 nroFila = 0
 
 for fila in table.find_all("tr"):
@@ -20,15 +21,29 @@ for fila in table.find_all("tr"):
         if nroCelda==0:
             name=celda.text
             print("Nombre:", name)
-        if nroCelda==4:
-            price=celda.text
-            print("Maximo:", price)
-        if nroCelda == 5:
-            price2=celda.text
-            print("Minimo:", price2)
+        if nroCelda==3:
+            porcent=celda.text
+            print("Porcentaje de Dif:", porcent)
         nroCelda+=1
-    nroFila=nroFila+1
+    nroFila+=1
 
+    with open('bolsa_ibex35.csv', 'a', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([name, porcent])
+    csv_file.close()
+
+with open('bolsa_ibex35.csv', 'r', encoding='utf-8') as csv_file2:
+    csv_reader = csv.reader(csv_file2)
+    next(csv_reader)
+    for line in csv_reader:
+        print(line)
+csv_file2.close()
+
+df = pd.read_csv('bolsa_ibex35.csv')
+
+headerList= ["Name" , "Porcentaje"]
+
+df.to_csv('bolsa_ibex35.csv',header=headerList, index=True)
 
 if __name__ == "__main__":
     print("Principal")
