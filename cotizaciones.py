63d9eccb3ef4bb1c5c6981ecb8cb21728112bf9b
Francisco Galindez <frangalindez7@gmail.com>
2021-09-13 01:12:21 -0300

    commit Fran_cotizaciones

--- cotizaciones.py
+++ cotizaciones.py
@@ -1,38 +1,45 @@
-if __name__ == '__main__':
-    print("Cotizaciones") 
+import matplotlib
 
 #Identificar las 2 cotizaciones de mayor ganancia y de mayor perdida.
 
-#si los cotizaciones están en una lista:
 
-nombres=[]
-max_ganancia=[]
-min_ganancia=[]
-
-mayor_maximo = max(max_ganancia)
-menor_minimo = min(min_ganancia)
-
-for i in range(len(max_ganancia)):
-    if mayor_maximo == max_ganancia[i]:
-        nombre_max = i
-    else:
-        pass
-
-for i in range(len(min_ganancia)):
-    if menor_minimo == min_ganancia[i]:
-        nombre_min = i
-    else:
-        pass
-print(nombres[nombre_max])
-print(nombres[nombre_min])
+#df = df.sort_values([info_header], ascending=True)     Codigo para tener la tabla antes hecha ordenada
 
 
 
 
-#en caso que debamos usar oop
-class bolsa_valores:
-    def __init__(self, nombre , max_ganancia , min_ganancia):
-        self.nombre = nombre
-        self.max_ganancia = max_ganancia
-        self.min_ganancia = min_ganancia
 
+def cotizaciones():
+    ganancias_perdidas = {[]}
+
+    largo_df = len(df)
+
+    mayor_perdida = df[0]       
+
+    for i in range(len(df)):
+        if df[i]== mayor_perdida:
+            df[i]["Name"] = nombre_mayor_perdida
+            ganancias_perdidas["nombre_ganancia"]= nombre_mayor_perdida
+            ganancias_perdidas["valor_ganancia"]= df[0]
+
+        if df[largo_df]== mayor_ganancia:
+            df[i]["Name"] = nombre_mayor_ganancia
+            ganancias_perdidas["nombre_ganancia"]= nombre_mayor_ganancia
+            ganancias_perdidas["valor_ganancia"]= df[largo_df]    
+        
+    
+    names = ganancias_perdidas["nombre_ganancia"]
+    porcentajes = ganancias_perdidas["valor_ganancia"]
+
+    matplotlib.rcParams["figure.figsize"] =10 , 5
+        pyplot.scatter(porcentajes, names, edgecolors="black", linewidths=1,alpha=0.75)
+        pyplot.title("  MAYORES PERDIDAS Y GANANCIAS")
+        pyplot.xlabel("PORCENTAJE")
+        pyplot.ylabel("EMPRESAS")
+
+        pyplot.tight_layout()
+        pyplot.show()
+
+
+if __name__ == '__main__':
+    print("Cotizaciones") 
