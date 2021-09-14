8d89045c631e796606edac1952a98e2c81a33d89
ADIA Viviana Farabollini <adia.vefarabollini@gmail.com>
2021-09-14 17:56:44 -0300

    reporte con datos de la url de la bolsa de madrid -porcentajes y comportamiento de acciones-

--- yahoo.py
+++ yahoo.py
@@ -5,14 +5,21 @@ Created on Tue Sep  7 18:33:41 2021
 @author: Oscar Ferreira
 """
 
-import yfinance as yf                                                          # Importamos la libereia yFinance
+# Importamos la libereia yFinance
+import yfinance as yf
 
-def yahoo (nonbreaccion):                                                      #Definimos la funcion Yahoo, el parametro es el nombre da la accion a consultar
-     
-    tipo= yf.Ticker(nonbreaccion)                                              # buscamos la accion
-    compra="Compra = "+str(tipo.info["ask"])                                   # guardamos el valor de Compra
-    venta="Venta ="+str(tipo.info["bid"])                                      # guardamos el valor de Venta
-    nombre=tipo.info["longName"]                                               # guardamos el nombre de la accion                                                        
-    return nombre, compra, venta                                               # Devuelve el nombre de la accion, el valor de compra y venta
-    
-    
+
+def yahoo(nonbreaccion):  # Definimos la funcion Yahoo, el parametro es el nombre da la accion a consultar
+
+    # buscamos la accion
+    tipo = yf.Ticker(nonbreaccion)
+    # guardamos el valor de Compra
+    compra = "Compra = "+str(tipo.info["ask"])
+    # guardamos el valor de Venta
+    venta = "Venta ="+str(tipo.info["bid"])
+    # guardamos el nombre de la accion
+    nombre = tipo.info["longName"]
+    # Devuelve el nombre de la accion, el valor de compra y venta
+    return nombre, compra, venta
+
+    print(TEF.MC)
